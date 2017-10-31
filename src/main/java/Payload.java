248fe9e95806f936ed0e1bdd863db4e6bfafbf5c
Daniele Perazzolo <dfperazzolo@gmail.com>
2017-10-30 21:00:38 -0700

    Implement basic decode and encode QR Code.

--- src/main/java/Payload.java
+++ src/main/java/Payload.java
@@ -1,9 +1,18 @@
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
 import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.QRCodeWriter;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
@@ -18,5 +27,30 @@ public class Payload {
 
         Path path = FileSystems.getDefault().getPath(filePath);
         MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+    }
+    
+    /* 
+    This method takes the text to be encoded, the width and height of the QR Code, 
+    and returns the QR Code in the form of a byte array.
+    */
+    public static BitMatrix getQRCodeImage(String text, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+        
+        return bitMatrix;
+    }
+    
+    public static String decodeQRCode(BufferedImage qrCodeimage) throws IOException {
+        BufferedImage bufferedImage = qrCodeimage;
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+        try {
+            Result result = new MultiFormatReader().decode(bitmap);
+            return result.getText();
+        } catch (NotFoundException e) {
+            System.out.println("There is no QR code in the image");
+            return null;
+        }
     }
 }
